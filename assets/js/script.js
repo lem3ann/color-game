84b305190cc9b23fc0117100a2aaaa8e1f5922ed
learngit <ltfv.lmn@gmail.com>
2026-05-01 00:43:26 +0400

    create:first structure

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// RANDOM COLOR GENERATOR
+let rgbHeader = document.querySelector("p.large-fs");
+
+function createRandomColor() {
+    let arr = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
+    rgbHeader.textContent = `RGB(${arr[0]},${arr[1]},${arr[2]})`;
+    return arr;
+}
+createRandomColor();
+// CREATE BOX (with color array)
+let mainContent = document.querySelector(".main-content");
+
+function createColorBox(array) {
+    let firstBox = document.createElement("div");
+    firstBox.classList.add("div-style");
+    firstBox.style.backgroundColor = `rgb${array[0]}`;
+    let secondBox = document.createElement("div");
+    secondBox.classList.add("div-style");
+    secondBox.style.backgroundColor = `rgb${array[1]}`;
+    let thirdBox = document.createElement("div");
+    thirdBox.classList.add("div-style");
+    thirdBox.style.backgroundColor = `rgb${array[2]}`;
+    mainContent.append(firstBox, secondBox, thirdBox);
+}
+createColorBox(arr);
