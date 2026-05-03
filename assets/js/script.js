ee9a8629607f86c0edc2a249be4002a0f8e27e2e
learngit <ltfv.lmn@gmail.com>
2026-05-03 21:02:42 +0400

    fix:easy and hard button bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,105 +2,167 @@
 let rgbHeader = document.querySelector("p.large-fs");
 
 function createRandomColor() {
-  let arr = [
-    Math.floor(Math.random() * 255),
-    Math.floor(Math.random() * 255),
-    Math.floor(Math.random() * 255),
-  ];
-  rgbHeader.textContent = `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`;
-  return arr;
+    let arr = [
+        Math.floor(Math.random() * 255) + 1,
+        Math.floor(Math.random() * 255) + 1,
+        Math.floor(Math.random() * 255) + 1,
+    ];
+
+    return arr;
 }
-createRandomColor();
+let trueColor = createRandomColor();
+rgbHeader.textContent = `rgb(${trueColor[0]}, ${trueColor[1]}, ${trueColor[2]})`;
+
 // CREATE BOX (with color array)
 let mainContent = document.querySelector(".main-content");
-let mixArr = [
-  [createRandomColor()],
-  [
-    Math.floor(Math.random() * 255),
-    Math.floor(Math.random() * 255),
-    Math.floor(Math.random() * 255),
-  ],
-  [
-    Math.floor(Math.random() * 255),
-    Math.floor(Math.random() * 255),
-    Math.floor(Math.random() * 255),
-  ],
-];
+let mainContent2 = document.querySelector(".main-content-2");
+
+function createRandomArr() {
+    let mixArrNew = [trueColor];
+    for (let index = 0; index < 5; index++) {
+        mixArrNew.unshift(createRandomColor());
+    }
+    return shuffle(mixArrNew);
+};
+
+
+function createRandomArrFor3Box() {
+    let mixArr = [trueColor];
+    for (let index = 0; index < 2; index++) {
+        mixArr.unshift(createRandomColor());
+    }
+    return $shuffle(mixArr);
+};
+
+console.log(createRandomArr());
+console.log(createRandomArrFor3Box());
+
 // shuffle array
 function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
+
+function $shuffle(array) {
+    for (let i = 2; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+// 2 defe shuffle function yaz birinci ilk 3-e qeder shuffle etsin,ikincisi 6 denesi arasinda 
 // create 3 colors with function
 function createColorBox(array) {
-  let firstBox = document.createElement("div");
-  firstBox.classList.add("div-style");
-  firstBox.style.backgroundColor = `rgb(${array[0]})`;
-  let secondBox = document.createElement("div");
-  secondBox.classList.add("div-style");
-  secondBox.style.backgroundColor = `rgb(${array[1]})`;
-  let thirdBox = document.createElement("div");
-  thirdBox.classList.add("div-style");
-  thirdBox.style.backgroundColor = `rgb(${array[2]})`;
-
-  mainContent.append(firstBox, secondBox, thirdBox);
+    mainContent.innerHTML = "";
+    for (let i = 0; i < 3; i++) {
+        let box = document.createElement("div");
+        box.classList.add("div-style");
+        box.style.backgroundColor = `rgb(${array[i]})`;
+        mainContent.appendChild(box);
+    }
 }
-// const sixColorArr = [];
 
-// create 6 color with function
-// function createSixColor() {}
+// ! create 6 colors with function
+function createSixColor(array) {
+    mainContent2.innerHTML = "";
+    for (let i = 0; i < 6; i++) {
+        let box = document.createElement("div");
+        box.classList.add("div-style");
+        box.style.backgroundColor = `rgb(${array[i]})`;
+        mainContent2.appendChild(box);
+    }
+}
 
-createColorBox(shuffle(mixArr));
+createColorBox(createRandomArrFor3Box());
+createSixColor(createRandomArr());
+// iki function eyni anda cagirilanda shuffle sehv isleyir ,izolyasiyali arrayi duzeldersen
 
 // NAVTABS ---easy ---hard
 let hardBtn = document.getElementsByClassName("hard")[0];
 let allTab = document.querySelectorAll(".toggle div");
+console.log(allTab);
+let allContent = document.querySelectorAll(".cards");
 
-allTab.forEach((element) => {
-  element.addEventListener("click", function () {
-    allTab.forEach((item) => {
-      item.classList.remove("active-class");
+for (let index = 0; index < allTab.length; index++) {
+    allTab[index].addEventListener("click", function () {
+        headerBg.style.backgroundColor = "steelblue";
+        comment.textContent = "";
+        for (let j = 0; j < allTab.length; j++) {
+            allContent[j].style.display = "none";
+            allTab[j].classList.remove("active-class");
+        }
+        allContent[index].style.display = "flex";
+        allTab[index].classList.add("active-class");
+        if (allTab[0].classList.contains("active-class")) {
+            changeHeaderAndBoxColor(3);
+        } else {
+            changeHeaderAndBoxColor(6);
+        }
     });
-    element.classList.add("active-class");
-  });
-});
+
+};
+
 // new colors button functionality
 let newColorBtn = document.getElementsByClassName("active-btn")[0];
 let comment = document.getElementsByClassName("comment")[0];
 let headerBg = document.getElementsByClassName("header")[0];
 
 // check  functionality
+setClickEffect();
 
-let allColorCards = document.querySelectorAll(".main-content div");
-allColorCards.forEach((element) => {
-  element.addEventListener("click", function () {
-    if (element.style.backgroundColor === rgbHeader.textContent) {
-      comment.textContent = "Correct";
-      headerBg.style.backgroundColor = `${element.style.backgroundColor}`;
-      newColorBtn.textContent = "PLAY AGAIN?";
-      for (const item of allColorCards) {
-        item.style.backgroundColor = `${element.style.backgroundColor}`;
-        item.style.opacity = 1;
-      }
-    } else {
-      comment.textContent = "Try Again";
-      element.style.opacity = 0;
-    }
-  });
-});
-function createSixColorBox() {
-  let firstBox = document.createElement("div");
-  firstBox.classList.add("div-style");
-  firstBox.style.backgroundColor = `rgb(${array[0]})`;
-  let secondBox = document.createElement("div");
-  secondBox.classList.add("div-style");
-  secondBox.style.backgroundColor = `rgb(${array[1]})`;
-  let thirdBox = document.createElement("div");
-  thirdBox.classList.add("div-style");
-  thirdBox.style.backgroundColor = `rgb(${array[2]})`;
-
-  mainContent.append(firstBox, secondBox, thirdBox);
+function setClickEffect() {
+    let allColorCards = document.querySelectorAll(".cards div");
+    allColorCards.forEach((element) => {
+        element.addEventListener("click", function () {
+            console.log("test");
+            if (element.style.backgroundColor === rgbHeader.textContent) {
+                comment.textContent = "Correct";
+                headerBg.style.backgroundColor = `${element.style.backgroundColor}`;
+                newColorBtn.textContent = "PLAY AGAIN?";
+                for (const item of allColorCards) {
+                    item.style.backgroundColor = `${element.style.backgroundColor}`;
+                    item.style.opacity = 1;
+                }
+            } else {
+                comment.textContent = "Try Again";
+                element.style.opacity = 0;
+            }
+        });
+    });
 }
+// New color button functionality
+function changeHeaderAndBoxColor(numb) {
+    trueColor = createRandomColor();
+    rgbHeader.textContent = `rgb(${trueColor[0]}, ${trueColor[1]}, ${trueColor[2]})`;
+    if (numb === 3) {
+        createColorBox(createRandomArrFor3Box());
+        setClickEffect();
+    } else {
+        createSixColor(createRandomArr());
+        setClickEffect();
+    }
+}
+// function for  reset
+newColorBtn.addEventListener("click", function () {
+    if (newColorBtn.textContent === "PLAY AGAIN?") {
+        newColorBtn.textContent = "NEW COLORS";
+        comment.textContent = "";
+    }
+    let active = document.querySelector(".toggle .active-class");
+    headerBg.style.backgroundColor = "steelblue";
+    if (active === allTab[0]) {
+        changeHeaderAndBoxColor(3);
+        setClickEffect();
+    } else {
+        changeHeaderAndBoxColor(6);
+        setClickEffect();
+    }
+});
+// set load effect
+let loadImg = document.getElementsByClassName("loading-img")[0];
+setTimeout(() => {
+    loadImg.style.display = "none";
+}, 4000);
